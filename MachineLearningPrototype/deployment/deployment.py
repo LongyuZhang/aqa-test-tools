9cc58c9979b28f443c682ff48e6cfd8a5b9220f0
Longyu Zhang <longyu.zhang@ibm.com>
2021-04-15 14:11:57 -0400

    test
    Signed-off-by: Longyu Zhang <longyu.zhang@ibm.com>

--- MachineLearningPrototype/deployment/deployment.py
+++ MachineLearningPrototype/deployment/deployment.py
@@ -13,7 +13,6 @@ import pickle
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.preprocess_data import remove_time_stamp
 
-
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
@@ -35,14 +34,15 @@ def predict_with_ml_model():
     console_output_df['received_content'] = [remove_time_stamp(content) for content in console_output_df['received_content']]
 
     # pre-process: vectorize input data with vectorizer from training
-    vectorizer_file_path = 'MachineLearningPrototype/data/TempData/vectorizer_file.pk'
+    ml_prototype_path = os.path.dirname(os.path.dirname(app.instance_path))
+    vectorizer_file_path = os.path.join(ml_prototype_path, 'data/TempData/vectorizer_file.pk')
     with open(vectorizer_file_path, "rb") as vectorizer_file:
         vectorizer = pickle.load(vectorizer_file)
     test_df = pd.DataFrame.sparse.from_spmatrix(vectorizer.transform(console_output_df.pop('received_content')), columns=vectorizer.get_feature_names())
     test_features = np.array(test_df)
 
     # pre-process: normalize the input features using the sklearn StandardScaler from training
-    standardScaler_file_path = 'MachineLearningPrototype/data/TempData/standardScaler_file.pk'
+    standardScaler_file_path = os.path.join(ml_prototype_path, 'data/TempData/standardScaler_file.pk')
     with open(standardScaler_file_path, "rb") as standardScaler_file:
         standardScaler = pickle.load(standardScaler_file)
     test_features_standardScaler = standardScaler.transform(test_features)
@@ -50,14 +50,14 @@ def predict_with_ml_model():
     # predict with saved ML model
     # parent_dir_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
     # ml_model_path = parent_dir_path + '/MLModel.joblib'
-    ml_model_path = 'MachineLearningPrototype/MLModel.joblib'
+    ml_model_path = os.path.join(ml_prototype_path, 'MLModel.joblib')
     savedModel = joblib.load(ml_model_path)
     predicted_label = savedModel.predict(test_features_standardScaler)
     print("savedModel Sample 0 predict label: ", predicted_label[0])
 
     # transform predicted lable number in to text with num2name dict from training
     # num2name_file_path = parent_dir_path + '/data/TempData/num2name_file'
-    num2name_file_path = 'MachineLearningPrototype/data/TempData/num2name_file'
+    num2name_file_path = os.path.join(ml_prototype_path, 'data/TempData/num2name_file')
     with open(num2name_file_path, "rb") as num2name_file:
         label_dict_num2name = pickle.load(num2name_file)
     predicted_result = label_dict_num2name[predicted_label[0]]
@@ -67,3 +67,6 @@ def predict_with_ml_model():
     output = {}
     output['result'] = predicted_result
     return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
